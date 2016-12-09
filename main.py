f63474412fac9da8e8efce16708a570834f6d4f4
yaki29 <yashjain.lnm@gmail.com>
2016-12-10 01:15:38 +0530

    anime added

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stencilview import StencilView 
 from kivy.lang import Builder
+from kivy.uix.scrollview import ScrollView
 from kivy.app import App
+from kivy.animation import Animation
+from kivy.animation import AnimationTransition
 Builder.load_string(
 '''
 <TransitionWidget>:
@@ -12,7 +15,7 @@ Builder.load_string(
 	page: 1
     Button:
         
-        background_color: 255, 0, 0, 1
+        background_color: 1, 1, 1, 0
         AnchorLayout:
         	size: self.parent.size
         	pos: self.parent.pos
@@ -32,22 +35,36 @@ Builder.load_string(
 		        		pos: self.pos
 		        		size: self.size
 	BoxLayout:
+		id: second_layout
 		orientation: 'vertical'
+		Button:
+			text: 'click'
+			on_press: root.anime()
+			size_hint: None, None
+			height: '50dp'
+			width: '50dp'
 		Label:
-			id: 'head'
+			id: head
 			text: 'This is to animate'
-			font_size: 20
+			font_size: 40
 			size_hint_y: None
 			height: '60dp'
+			pos_hint_x: None
+			pos_hint_y: None
+			markup: True
+			halign: 'left'
+			valign: 'middle'
 
 		ScrollView:
+			bar_color: 1, 0 , 0, 1
+			bar_width: 4
 			Label:
 		        text: 'my name is Yash '*100
 		        font_size: 50
 		        text_size: self.width, None
 		        size_hint_y: None
 		        height: self.texture_size[1]
-		        #on_scroll_start: cross.background_color = (1,0,0,00.5)
+		        # on_scroll_start: cross.background_color = (1,0,0,00.5)
 		        # background_color: 255, 0, 0, 1
 		        AnchorLayout:
 		        	size: self.parent.size
@@ -55,7 +72,7 @@ Builder.load_string(
 		        	anchor_x: 'right'
 		        	anchor_y: 'top'
 		        	Button:
-		        		id: 'cross'
+		        		id: cross
 		        		size_hint: None, None
 		        		height: '60dp'
 		        		width: '60dp'
@@ -73,7 +90,10 @@ Builder.load_string(
 ''')
 
 class TransitionWidget(PageLayout):
-	pass
+	def anime(self):
+		lab = self.ids['head']
+		ani = Animation(pos=(500,800), size=(10, 10), t = 'linear',duration=0.5)
+		ani.start(lab)
 class ScreenApp(App):
 
 	def build(self):
