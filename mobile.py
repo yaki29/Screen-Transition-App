5104264da97e365d0db410c22e253e0a7e6a0deb
yaki29 <yashjain.lnm@gmail.com>
2016-12-17 02:05:24 +0530

    coordinates modified

--- mobile.py
+++ mobile.py
@@ -27,27 +27,31 @@ Builder.load_string(
         		height: '60dp'
         		width: '60dp'
         		on_press: root.manager.current = 'second'
-        		background_color: 0, 0.99, 0.59, 1
+        		background_color: 0, 0.99, 0 ,1
 <SecondPage>:	
 	BoxLayout:
 		id: second_layout
 		orientation: 'vertical'
-		# Button:
-		# 	text: 'anime'
-		# 	on_press: root.anime()
-		# 	size_hint: None, None
-		# 	height: '50dp'
-		# 	width: '50dp'
-		# 	background_color: 0, 0.99, 0 ,1
+		Button:
+			text: 'Back'
+			on_press: root.manager.current = 'start'
+			size_hint: None, None
+			height: '50dp'
+			width: '50dp'
+			background_color: 0, 0.99, 0 ,1
 
 		Label:
 			id: head
 			text: 'This is to animate'
 			font_size: 40
+			# pos_hint_x: None
+			# pos_hint_y: None
+			pos: - self.parent.width*0.30 + scroller.scroll_y * 150,self.parent.height*0.86 + scroller.scroll_y * 50
 			size_hint_y: None
 			height: '110dp'
 			halign: 'left'
 			valign: 'top'
+
 
 		ScrollView:
 			id: scroller
@@ -57,28 +61,28 @@ Builder.load_string(
 			on_scroll_start: root.animeup(self.scroll_y)
 			# on_scroll_start: root.animeup()
 			Label:
-		        text: 'my name is Yash '*40
+		        text: 'my name is Yash '*100
 		        font_size: 50
 		        text_size: self.width, None
 		        size_hint_y: None
 		        height: self.texture_size[1]
 		        
 		        
-		        # background_color: 1, 0, 0, 1
-		        AnchorLayout:
-		        	size: self.parent.size
-		        	pos: self.parent.pos
-		        	anchor_x: 'right'
-		        	anchor_y: 'top'
-		        	Button:
-		        		id: cross
-		        		text: "back"
-		        		size_hint: None, None
-		        		height: '60dp'
-		        		width: '60dp'
-		        		on_press: root.manager.current = 'start'
-		        		on_press: root.change_base()
-		        		background_color: 0, 0.99, 0.59, 1
+		        # # background_color: 1, 0, 0, 1
+		        # AnchorLayout:
+		        # 	size: self.parent.size
+		        # 	pos: self.parent.pos
+		        # 	anchor_x: 'right'
+		        # 	anchor_y: 'top'
+		        # 	Button:
+		        # 		id: cross
+		        # 		text: "back"
+		        # 		size_hint: None, None
+		        # 		height: '60dp'
+		        # 		width: '60dp'
+		        # 		on_press: root.manager.current = 'start'
+		        # 		on_press: root.change_base()
+		        # 		background_color: 0, 0.99, 0.59, 1
 
 ''')
 
@@ -105,8 +109,9 @@ class SecondPage(Screen):
 		# if flag == 0:
 		scr = self.ids['scroller']
 		lab = self.ids['head']
-		lab.pos[1] =  450 + scr.scroll_y * 50
-		lab.pos[0] =  -230 + 10*(7.73**scr.scroll_y)
+
+		lab.pos[1] =  lab.parent.height*0.86 + scr.scroll_y * 50
+		lab.pos[0] =  - lab.parent.width*0.30 + scr.scroll_y * 150
 		print scr.scroll_y
 		# ani = Animation(pos=(-self.width*0.30,self.height*0.905), size=(70,70),duration=0.5) + Animation(pos=(-self.width*0.05,self.height*0.93), t = 'linear',duration=1)
 		# ani.start(lab)
@@ -124,7 +129,6 @@ global previous
 previous = -1
 
 class ScreenApp(App):
-	
 	def build(self):
 		return	sm
 ScreenApp().run()
